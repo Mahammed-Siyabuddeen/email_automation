21de7734c018bef8acdd8fa36dfee73f80eac910
mahamme-siyabuddeen <mohd.sihab615@gmail.com>
2024-11-24 22:19:25 +0530

    function to fecth message from openai

---
+++ src/controllers/generateMessage.ts
@@ -0,0 +1,28 @@
+import OpenAI from "openai";
+
+
+async function generateMessage(message: string) {
+
+    try {
+        const openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY,
+            baseURL: process.env.OPENAI_BASE_URL
+        });
+
+        const response = await openai.chat.completions.create({
+            messages: [{
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            { role: 'user', content: `generate replay answer email label and body for these qestion :${message}` },],
+            model: 'cosmosrp',
+        });
+
+        return response['choices'][0]['message']['content'];
+    } catch (error) {
+        return "I dont know";
+    }
+
+}
+
+export default generateMessage;
